220492ed75dd68eef19d62dbb31948688490d501
BHyeonKim <rlaqhguse@gmail.com>
2026-05-11 02:50:10 +0900

    feat(search): SearchBar 컴포넌트 구현 및 홈 화면 적용

--- src/pages/home/ui/HomePage.tsx
+++ src/pages/home/ui/HomePage.tsx
@@ -1,8 +1,10 @@
+import SearchBar from '@/features/search/ui/SearchBar';
 import { WeatherMap } from '@/widgets/map';
 
 const HomePage = () => {
 	return (
 		<div className="h-full w-full">
+			<SearchBar className="fixed top-4 left-4 z-10" />
 			<WeatherMap />
 		</div>
 	);
