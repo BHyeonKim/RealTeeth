66e42a2eaf3134940466bb4006be72f23b80cad6
BHyeonKim <rlaqhguse@gmail.com>
2026-05-11 00:47:26 +0900

    feat(whether): React Query 프로바이더 설정

--- src/main.tsx
+++ src/main.tsx
@@ -6,12 +6,15 @@ import Router from '@/app/Router';
 
 import './app/styles/index.css';
 import { NavermapsProvider } from 'react-naver-maps';
+import { QueryProvider } from './app/providers/QueryProvider';
 
 // biome-ignore lint/style/noNonNullAssertion: root element always exists
 createRoot(document.getElementById('root')!).render(
 	<StrictMode>
-		<NavermapsProvider ncpKeyId={import.meta.env.VITE_NAVER_MAP_CLIENT_ID}>
-			<RouterProvider router={Router} />
-		</NavermapsProvider>
+		<QueryProvider>
+			<NavermapsProvider ncpKeyId={import.meta.env.VITE_NAVER_MAP_CLIENT_ID}>
+				<RouterProvider router={Router} />
+			</NavermapsProvider>
+		</QueryProvider>
 	</StrictMode>,
 );
