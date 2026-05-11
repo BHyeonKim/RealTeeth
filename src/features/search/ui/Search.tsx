18d2c05a0e47967bfbea442acd93d979baf75b6e
BHyeonKim <rlaqhguse@gmail.com>
2026-05-11 19:06:36 +0900

    fix(search): 상세 페이지 진입 시 검색 결과 목록 숨김

--- src/features/search/ui/Search.tsx
+++ src/features/search/ui/Search.tsx
@@ -1,3 +1,4 @@
+import { useMatch } from 'react-router';
 import { twMerge } from 'tailwind-merge';
 
 import useSearchLocation from '../hooks/useSearchLocation';
@@ -10,11 +11,14 @@ type SearchProps = {
 
 const Search = ({ className }: SearchProps) => {
 	const { filteredDistricts, handleSearch, query } = useSearchLocation();
+	const isDetailPage = useMatch('/detail');
 
 	return (
 		<div className={twMerge('w-full max-w-95', className)}>
 			<SearchBar onChange={handleSearch} value={query} />
-			<SearchItemList className="mt-2" items={filteredDistricts} />
+			{!isDetailPage && (
+				<SearchItemList className="mt-2" items={filteredDistricts} />
+			)}
 		</div>
 	);
 };
