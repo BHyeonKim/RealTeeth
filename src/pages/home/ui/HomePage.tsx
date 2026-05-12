23fb9917b7f2a49e8b8e356874d79050e707fe6f
BHyeonKim <rlaqhguse@gmail.com>
2026-05-12 15:04:16 +0900

    feat(favorite): 홈/상세 페이지 즐겨찾기 연동
    - HomePage: FavoriteToggleButton, Favorite 사이드바 추가
    - FavoriteButton: 즐겨찾기 추가/됨 상태 표시
    - DetailPage: FavoriteButton에 현재 위치 Favorite 전달

--- src/pages/home/ui/HomePage.tsx
+++ src/pages/home/ui/HomePage.tsx
@@ -1,12 +1,16 @@
 import { Outlet } from 'react-router';
+
+import { Favorite, FavoriteToggleButton } from '@/features/favorite';
 import Search from '@/features/search/ui/Search';
 import { WeatherMap } from '@/widgets/map';
 
 const HomePage = () => {
 	return (
-		<div className="h-full w-full">
-			<Search className="fixed top-4 left-4 z-10" />
+		<div className="relative h-full w-full">
+			<FavoriteToggleButton className="fixed top-4 left-4 z-10" />
+			<Search className="fixed top-18 left-4 z-10" />
 			<WeatherMap />
+			<Favorite />
 			<Outlet />
 		</div>
 	);
