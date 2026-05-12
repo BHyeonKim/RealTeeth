432c4b8b43674ba0452f346fe403eeb2317b9dd4
BHyeonKim <rlaqhguse@gmail.com>
2026-05-12 20:52:17 +0900

    fix(search): SearchItemList JSX 반환 구조 오류 수정

--- src/features/search/ui/SearchItemList.tsx
+++ src/features/search/ui/SearchItemList.tsx
@@ -14,20 +14,24 @@ const SearchItemList = ({
 	onItemClick,
 }: SearchItemListProps) => {
 	return (
-		<ul
-			className={twMerge(
-				'scrollbar-hide absolute top-full right-0 left-0 z-50 max-h-90 w-full max-w-95 overflow-hidden overflow-y-scroll rounded-2xl border border-white/15 bg-white/10 shadow-[0_16px_40px_-4px_rgba(0,0,0,0.5)] backdrop-blur-xl',
-				className,
+		<>
+			{items.length > 0 && (
+				<ul
+					className={twMerge(
+						'scrollbar-hide absolute top-full right-0 left-0 z-50 max-h-90 w-full max-w-95 overflow-hidden overflow-y-scroll rounded-2xl border border-white/15 bg-white/10 shadow-[0_16px_40px_-4px_rgba(0,0,0,0.5)] backdrop-blur-xl',
+						className,
+					)}
+				>
+					{items.map((item) => (
+						<SearchItem
+							key={`${item.name}-${item.address}`}
+							{...item}
+							onItemClick={onItemClick}
+						/>
+					))}
+				</ul>
 			)}
-		>
-			{items.map((item) => (
-				<SearchItem
-					key={`${item.name}-${item.address}`}
-					{...item}
-					onItemClick={onItemClick}
-				/>
-			))}
-		</ul>
+		</>
 	);
 };
 
