2e13b69771f70bd30f30b211f3422c078fedf17f
BHyeonKim <rlaqhguse@gmail.com>
2026-05-12 17:19:08 +0900

    style(responsive): 홈 검색 영역 반응형 배치 적용

--- src/pages/home/ui/HomePage.tsx
+++ src/pages/home/ui/HomePage.tsx
@@ -3,15 +3,22 @@ import { Outlet, useLocation } from 'react-router';
 
 import { FavoriteToggleButton } from '@/features/favorite';
 import Search from '@/features/search/ui/Search';
+import useDeviceSize from '@/shared/hooks/useDeviceSize';
 import { WeatherMap } from '@/widgets/map';
 
 const HomePage = () => {
 	const location = useLocation();
+	const device = useDeviceSize();
+	const isDesktop = device === 'desktop';
 
 	return (
 		<div className="relative h-full w-full">
-			<FavoriteToggleButton className="fixed top-4 left-4 z-10" />
-			<Search className="fixed top-18 left-4 z-10" />
+			<div
+				className={`fixed top-4 z-10 flex w-full max-w-95 flex-col gap-3 ${isDesktop ? 'left-4' : 'left-1/2 -translate-x-1/2'}`}
+			>
+				{isDesktop && <FavoriteToggleButton />}
+				<Search />
+			</div>
 			<WeatherMap />
 			<AnimatePresence>
 				<Outlet key={location.pathname} />
