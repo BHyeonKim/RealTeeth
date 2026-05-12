b549b11d5f78a9329d64f9af963c3727c1478f67
BHyeonKim <rlaqhguse@gmail.com>
2026-05-12 19:51:02 +0900

    chore: bundle visualizer 추가

--- vite.config.ts
+++ vite.config.ts
@@ -2,10 +2,11 @@ import path from 'node:path';
 import svgr from '@svgr/rollup';
 import tailwindcss from '@tailwindcss/vite';
 import react from '@vitejs/plugin-react';
+import { visualizer } from 'rollup-plugin-visualizer';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-	plugins: [tailwindcss(), react(), svgr()],
+	plugins: [visualizer(), tailwindcss(), react(), svgr()],
 	resolve: {
 		alias: {
 			'@': path.resolve(__dirname, './src'),
